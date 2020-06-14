2668e835e4ea60ae860de77441e71971ae1e85c3
Andrew Durrant <andrew.durrant75@gmail.com>
2020-06-13 20:54:14 -0600

    Updates

---
+++ precache-manifest.749ab7f79a254e86c091bb036dbd8ac4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222402bae65c778a67023e186f5ebc53",
+    "url": "/open-mic-static-client/index.html"
+  },
+  {
+    "revision": "6cc150f553c67872e84d",
+    "url": "/open-mic-static-client/static/css/main.3f35aacd.chunk.css"
+  },
+  {
+    "revision": "89e3b0e84759f4c93350",
+    "url": "/open-mic-static-client/static/js/2.42628028.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/open-mic-static-client/static/js/2.42628028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cc150f553c67872e84d",
+    "url": "/open-mic-static-client/static/js/main.dcbf99da.chunk.js"
+  },
+  {
+    "revision": "6be2d479fafec240d9ee",
+    "url": "/open-mic-static-client/static/js/runtime-main.1ee0386f.js"
+  }
+]);
